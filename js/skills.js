31c2a1982d934f4738ad60ec4cb17a73acb0d4ec
Joosunghoon <151600397+sunghoon1209@users.noreply.github.com>
2024-06-06 02:48:08 +0900

    Add files via upload

--- js/skills.js
+++ js/skills.js
@@ -1,23 +1,19 @@
-
 /* 버튼 */
 function buttonClick(event) {
     let button = event.target;
     let skillsItems = document.querySelectorAll('.skills__item');
     let toolsItems = document.querySelectorAll('.tools__item');
 
-    if (button.classList.contains('skills__button-skills'))
-        {
-            skillsItems.forEach(item=> item.style.display="flex");
-            toolsItems.forEach(item=> item.style.display="none");
-            ;
+    if (button.classList.contains('skills__button-skills')) {
+        skillsItems.forEach(item => item.style.display = "flex");
+        toolsItems.forEach(item => item.style.display = "none");;
 
-        }
-    else{
-        skillsItems.forEach(item=> item.style.display="none");
-        toolsItems.forEach(item=> item.style.display="flex");
-      
+    } else {
+        skillsItems.forEach(item => item.style.display = "none");
+        toolsItems.forEach(item => item.style.display = "flex");
+
 
     };
-    
+
 };
 
