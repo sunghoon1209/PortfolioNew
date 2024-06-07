77b0b0c18f404906c5c99ee37a5be666b6e992a8
Joosunghoon <151600397+sunghoon1209@users.noreply.github.com>
2024-06-07 14:20:40 +0900

    Add files via upload

--- js/works.js
+++ js/works.js
@@ -10,7 +10,7 @@ window.onload = function () {
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
+
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -21,55 +21,53 @@ window.onload = function () {
         },
         // 2
         {
-            title: "포트폴리오",
+            title: "포트폴리오(구버전)",
             subttitle: "Web | 퍼블리싱 100%",
-            description: "그 동안의 작업물들과 제 자신을 소개하기 위한 포트폴리오 페이지입니다..",
-            pageThumb: "./images/portfolio.jpg",
-            url: "https://sunghoon1209.github.io/portfolio/",
+            description: "기존에 작업했던 작업물들과 저를 소개하는 페이지입니다.",
+            pageThumb: "./images/portfull.jpg",
+            url: "https://sunghoon1209.github.io/portfolio/landingpage",
             codeUrl: "https://github.com/sunghoon1209/portfolio/archive/refs/heads/main.zip",
-            developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
-            skills: "HTML SCSS JavaScript",
-            feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature1stImg: "./images/portfolioN_feature.png",
-            feature2nd: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature2ndImg: "./images/portfolioN_feature.png",
-            feedback: "동일한 구조의 header지만 그리드를 margin 0 auto로 잡았기때문에 스크롤바가 있는 페이지와 없는 페이지의 중앙 너비가 다르게 보임"
+            developVision: "기본적인 구조는 약간의 애니메이션이 적용된 Landing page에서 포트폴리오 페이지로 이동되게 제작하였습니다. 포트폴리오 해당 페이지를 제작할때 사용자 경험을 향상하고자 노력했습니다. 예를들어 저의 작업물을 소개하는 팝업 페이지에서 페이지썸네일의 스크롤이 있는 경우에 디자인적인 측면을 고려하여 scroll은 보이지 않게 숨겼습니다. 사용자 입장에서는 해당 영역이 스크롤이 가능하다는 것을 알게하기위해서 우측 하단에 마우스 스크롤 이미지를 넣었습니다. ",
+
+            skills: "HTML SCSS JavaScript jQuery",
+            feature1st: "Contact 영역 하단 폼을 제 구글 계정 e-mail에 연동하여 제출시 사용자가 입력한 내용이 이메일로 전송되게 제작하였습니다.",
+            feature1stImg: "./images/portfolio_feature.png",
+            feature2nd: null,
+            feature2ndImg: "",
+            feedback: "코드를 작성할때 class를 너무 임의로 순간적으로 작성하여 코드 가독성이 부족 유지보수하기 열악"
 
         },
         // 1
         {
             title: "Gucci",
             subttitle: "반응형 | 퍼블리싱 100%",
-            description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/gucci.jpg",
-            url: "https://sunghoon1209.github.io/PortfolioNew/",
-            codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
-            developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
-            skills: "HTML SCSS JavaScript",
-            feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature1stImg: "./images/portfolioN_feature.png",
-            feature2nd: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature2ndImg: "./images/portfolioN_feature.png",
-            feedback: "동일한 구조의 header지만 그리드를 margin 0 auto로 잡았기때문에 스크롤바가 있는 페이지와 없는 페이지의 중앙 너비가 다르게 보임"
+            description: "기존 구찌 페이지를 보고 클론 코딩하여 제작하였습니다.",
+            pageThumb: "./images/guccifull.jpg",
+            url: "https://sunghoon1209.github.io/Gucci/",
+            codeUrl: "https://github.com/sunghoon1209/Gucci/archive/refs/heads/main.zip",
+            developVision: "SASS를 공부하고 나서 SASS에 대한 숙련도를 향상시키기 위해서 SASS를 사용하여 제작했습니다. 또한 반응형으로 제작하였고 slick.js와 wow.js등의 플러그인 등을 활용하여 기능을 추가했습니다. 추가적으로 jQuery의 숙련도 향상을 위해 jQuery로 제작되었습니다.",
+            skills: "HTML SCSS jQuery",
+            feature1st: "jQuery를 이용하여 비디오 박스를 만들고 그 안에 재생/음소거 버튼을 만들어서 클릭시 버튼의 모양이 변경되면서 재생/음소거의 기능도 구현",
+            feature1stImg: "./images/gucci_feature.png",
+            feature2nd: "",
+            feature2ndImg: "",
+            feedback: "wow.js에서 position 문제로 등장 위치가 잘못 적용되는듯 함"
 
         },
         // 1
         {
             title: "데브시스터즈",
             subttitle: "Web | 퍼블리싱 100%",
-            description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
-            url: "https://sunghoon1209.github.io/PortfolioNew/",
-            codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
+            description: "Devsisters의 페이지를 참고하여 새롭게 디자인하여 제작하였습니다.",
+            pageThumb: "./images/devFull.jpg",
+            url: "https://sunghoon1209.github.io/Devsisters/",
+            codeUrl: "https://github.com/sunghoon1209/Devsisters/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
-            skills: "HTML SCSS JavaScript",
-            feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature1stImg: "./images/portfolioN_feature.png",
-            feature2nd: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature2ndImg: "./images/portfolioN_feature.png",
+            skills: "HTML SCSS JavaScript jQuery",
+            feature1st: "지원하기 버튼을 css에서 해당 버튼의 가상요소의 크기, 반지름, 위치 등을 변수로 지정하고 자바스크립트에서 마우스 올리는 event시에 해당 마우스의 x,y좌표를 계산하여 동적으로  ripple 효과를 구현했습니다.",
+            feature1stImg: "./images/devsisters_feature.png",
+            feature2nd: "",
+            feature2ndImg: "",
             feedback: "동일한 구조의 header지만 그리드를 margin 0 auto로 잡았기때문에 스크롤바가 있는 페이지와 없는 페이지의 중앙 너비가 다르게 보임"
 
         },
@@ -77,17 +75,16 @@ window.onload = function () {
         {
             title: "삼양식품",
             subttitle: "Web | 퍼블리싱 100%",
-            description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
-            url: "https://sunghoon1209.github.io/PortfolioNew/",
-            codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
+            description: "삼양식품 페이지에 대한 클론 코딩입니다.",
+            pageThumb: "./images/samyangfull.jpg",
+            url: "https://sunghoon1209.github.io/samyang/",
+            codeUrl: "https://github.com/sunghoon1209/samyang/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
-            feature2nd: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
-            feature2ndImg: "./images/portfolioN_feature.png",
+            feature2nd: "",
+            feature2ndImg: "",
             feedback: "동일한 구조의 header지만 그리드를 margin 0 auto로 잡았기때문에 스크롤바가 있는 페이지와 없는 페이지의 중앙 너비가 다르게 보임"
 
         },
@@ -96,11 +93,10 @@ window.onload = function () {
             title: "ITSIX",
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/itsixfullpage.jpeg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -114,11 +110,10 @@ window.onload = function () {
             title: "Petfriends",
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/petfriendsFull.jpg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -132,11 +127,11 @@ window.onload = function () {
             title: "H.point",
             subttitle: "Web | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/hpointFull.jpg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
+
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -150,11 +145,11 @@ window.onload = function () {
             title: "Hanhwa Chemical",
             subttitle: "Web | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/hanhwaFull.jpg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
+
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -164,16 +159,16 @@ window.onload = function () {
 
         },
         // 1
-      
+
         {
             title: "Ground X",
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/groundXfull.jpg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
+
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -187,11 +182,11 @@ window.onload = function () {
             title: "3D Site",
             subttitle: "Web | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/3d.gif",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
+
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -205,11 +200,10 @@ window.onload = function () {
             title: "SK Picglobal",
             subttitle: "Web | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/skPicFull.jpg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -223,11 +217,11 @@ window.onload = function () {
             title: "Video",
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/battlenet_figma.svg",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
-            img: "./images/portfolio.jpg",
+
             skills: "HTML SCSS JavaScript",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -240,11 +234,11 @@ window.onload = function () {
             title: "배틀넷",
             subttitle: "Figma | 디자인 100% 인터렉션 100%",
             description: "Battlent 어플리케이션을 새롭게 디자인하여 Figma로 인터렉션 구현",
-            pageThumb: "./images/portfolioN_pc.png",
+            pageThumb: "./images/battlenet_figma.svg",
             url: "https://www.figma.com/proto/4eJQhVwGF52em5G7bdYIwA/%EC%A3%BC%EC%84%B1%ED%9B%88-%EB%B0%B0%ED%8B%80%EB%84%B7-UIUX%EB%94%94%EC%9E%90%EC%9D%B8%ED%8F%89%EA%B0%80?node-id=1-4&t=nVJKIq1j5pa0Tf3q-1&scaling=scale-down&page-id=0%3A1&starting-point-node-id=98%3A403",
             codeUrl: "https://www.figma.com/design/4eJQhVwGF52em5G7bdYIwA/%EC%A3%BC%EC%84%B1%ED%9B%88-%EB%B0%B0%ED%8B%80%EB%84%B7-UIUX%EB%94%94%EC%9E%90%EC%9D%B8%ED%8F%89%EA%B0%80?node-id=98-403&t=zgDQvDO4OXigK6Nv-1",
             developVision: "Figma의 사용법과 UIUX에 대한 이해도를 향상하기 위해 제작하였습니다. ",
-            img: "./images/portfolio.jpg",
+
             skills: "Figma",
             feature1st: "Class 지정할때 BEM 방법으로 가독성 향상 및 유지보수 용이화",
             feature1stImg: "./images/portfolioN_feature.png",
@@ -321,25 +315,29 @@ window.onload = function () {
     });
 
     // 팝업창 닫기
-    let popUpClose = document.querySelector('.pop-up__button__list');
-    popUpClose.addEventListener("click", function () {
-        let wrapper = document.querySelector('.portfolio__wrapper');
-        wrapper.classList.remove('portfolio__wrapper--hide');
+    let popUpClose = document.querySelectorAll('.pop-up__button__list');
+    popUpClose.forEach(function (button) {
+        button.addEventListener("click", function () {
+            let wrapper = document.querySelector('.portfolio__wrapper');
+            wrapper.classList.remove('portfolio__wrapper--hide');
 
-        let popUp = document.querySelector(".popup");
-        popUp.classList.remove('popup--show');
+            let popUp = document.querySelector(".popup");
+            popUp.classList.remove('popup--show');
+        })
     });
 
+
     // next 버튼 누를 때
-    let nextBtn = document.querySelector('.pop-up__button__next');
-    nextBtn.addEventListener("click", function () {
-        // 인덱스를 증가시키고 배열의 길이를 넘으면 0으로
-        currentIndex = (currentIndex + 1) % popUpInfoArray.length;
+    let nextBtn = document.querySelectorAll('.pop-up__button__next');
+    nextBtn.forEach(function (button) {
+        button.addEventListener("click", function () {
+            // 인덱스를 증가시키고 배열의 길이를 넘으면 0으로
+            currentIndex = (currentIndex + 1) % popUpInfoArray.length;
 
-        // 다음 데이터 설정
-        let data = popUpInfoArray[currentIndex];
+            // 다음 데이터 설정
+            let data = popUpInfoArray[currentIndex];
 
-        popUpTitle.textContent = data.title;
+            popUpTitle.textContent = data.title;
             // 소제목 변경
             popUpSubtitle.textContent = data.subttitle;
             // 한줄 소개
@@ -364,5 +362,6 @@ window.onload = function () {
             popUpFeature2ndImg.setAttribute("src", data.feature2ndImg);
             // 개선사항
             popUpFeedback.textContent = data.feedback;
+        })
     });
 };
