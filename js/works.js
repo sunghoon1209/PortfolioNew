581bf751eceae94cb6cba02388d8988fc8cbb958
Joosunghoon <151600397+sunghoon1209@users.noreply.github.com>
2024-06-09 17:20:24 +0900

    Add files via upload

--- js/works.js
+++ js/works.js
@@ -7,6 +7,7 @@ window.onload = function () {
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "기존 포트폴리오에 대한 피드백을 담아 새롭게 제작한 페이지입니다.",
             pageThumb: "./images/portfolioN_pc.webp",
+            pageTumbAlt: "포트폴리오 썸네일",
             url: "https://sunghoon1209.github.io/PortfolioNew/",
             codeUrl: "https://github.com/sunghoon1209/PortfolioNew/archive/refs/heads/main.zip",
             developVision: "기존에 작업했던 포트폴리오에 대해서 스스로 피드백을 해본 결과, HTML작성할때 class를 규칙성 없이 지정하다보니 코드의 가독성이 떨어지고 유지보수가 어려워서 이를 향상 시키는 연습을 하기위해 제작했습니다. 또한 기존의 포트폴리오에서는 반응형 페이지가 아닌 웹 전용 페이지임에 아쉬움을 느꼈고 Pc,Tablet,Mobile의 3단 구조로 새롭게 제작했습니다.",
@@ -25,6 +26,7 @@ window.onload = function () {
             subttitle: "Web | 퍼블리싱 100%",
             description: "기존에 작업했던 작업물들과 저를 소개하는 페이지입니다.",
             pageThumb: "./images/portfull.webp",
+            pageTumbAlt: "포트폴리오 구버전 썸네일",
             url: "https://sunghoon1209.github.io/portfolio/landingpage",
             codeUrl: "https://github.com/sunghoon1209/portfolio/archive/refs/heads/main.zip",
             developVision: "기본적인 구조는 약간의 애니메이션이 적용된 Landing page에서 포트폴리오 페이지로 이동되게 제작하였습니다. 포트폴리오 해당 페이지를 제작할때 사용자 경험을 향상하고자 노력했습니다. 예를들어 저의 작업물을 소개하는 팝업 페이지에서 페이지썸네일의 스크롤이 있는 경우에 디자인적인 측면을 고려하여 scroll은 보이지 않게 숨겼습니다. 사용자 입장에서는 해당 영역이 스크롤이 가능하다는 것을 알게하기위해서 우측 하단에 마우스 스크롤 이미지를 넣었습니다. ",
@@ -42,7 +44,8 @@ window.onload = function () {
             title: "Gucci",
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "기존 구찌 페이지를 보고 클론 코딩하여 제작하였습니다.",
-            pageThumb: "./images/gucciFull.webp",
+            pageThumb: "./images/guccifull.webp",
+            pageTumbAlt: "구찌 썸네일",
             url: "https://sunghoon1209.github.io/Gucci/",
             codeUrl: "https://github.com/sunghoon1209/Gucci/archive/refs/heads/main.zip",
             developVision: "SASS를 공부하고 나서 SASS에 대한 숙련도를 향상시키기 위해서 SASS를 사용하여 제작했습니다. 또한 반응형으로 제작하였고 slick.js와 wow.js등의 플러그인 등을 활용하여 기능을 추가했습니다. 추가적으로 jQuery의 숙련도 향상을 위해 jQuery로 제작되었습니다.",
@@ -60,7 +63,8 @@ window.onload = function () {
             title: "데브시스터즈",
             subttitle: "Web | 퍼블리싱 100%",
             description: "Devsisters의 페이지를 참고하여 새롭게 디자인하여 제작하였습니다.",
-            pageThumb: "./images/devFull.webp",
+            pageThumb: "./images/devfull.webp",
+            pageTumbAlt: "데브시스터즈 썸네일",
             url: "https://sunghoon1209.github.io/Devsisters/",
             codeUrl: "https://github.com/sunghoon1209/Devsisters/archive/refs/heads/main.zip",
             developVision: " 자바스크립트와 제이쿼리를 이용하여 다양한 이벤트를 구현하는데 집중했습니다. ",
@@ -79,6 +83,7 @@ window.onload = function () {
             subttitle: "Web | 퍼블리싱 100%",
             description: "삼양식품 페이지에 대한 클론 코딩입니다.",
             pageThumb: "./images/samyangfull.webp",
+            pageTumbAlt: "삼양식품 썸네일",
             url: "https://sunghoon1209.github.io/samyang/",
             codeUrl: "https://github.com/sunghoon1209/samyang/archive/refs/heads/main.zip",
             developVision: "jQuery와 플러그인을 사용하여 동적인 효과를 주는데 집중했습니다.",
@@ -97,6 +102,7 @@ window.onload = function () {
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "ITSIX페이지를 수정하여 클론 코딩하였습니다.",
             pageThumb: "./images/itsixfullpage.webp",
+            pageTumbAlt: "itsix 썸네일",
             url: "https://sunghoon1209.github.io/itsix/",
             codeUrl: "https://github.com/sunghoon1209/itsix/archive/refs/heads/main.zip",
             developVision: "• PC/Mobile의 2단구조 반응형으로 제작했습니다. jQuery를 이용해 간단한 이벤트를 구현했습니다.",
@@ -115,6 +121,7 @@ window.onload = function () {
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "모바일 형식으로만 존재하던 펫프렌즈를 컨셉에 맞게 웹 페이지로 새로 만들었습니다.",
             pageThumb: "./images/petfriendsfull.webp",
+            pageTumbAlt: "펫프렌즈 썸네일",
             url: "https://sunghoon1209.github.io/petfriends/",
             codeUrl: "https://github.com/sunghoon1209/petfriends/archive/refs/heads/main.zip",
             developVision: " display : grid 구조를 공부하기 위해서 해당 구조로 제작했습니다. 또한 기존에 width와 margin: 0 auto 로 조절하던 중앙 그리드를 padding으로 기존과 다른 방식으로 조절했습니다. 추가적으로 swiper slider 플러그인을 사용해서 효과를 줬습니다.  ",
@@ -132,7 +139,8 @@ window.onload = function () {
             title: "H.point",
             subttitle: "Web | 퍼블리싱 100%",
             description: "H.point 페이지를 클론 코딩했습니다.",
-            pageThumb: "./images/hpointFull.webp",
+            pageThumb: "./images/hpointfull.webp",
+            pageTumbAlt: "h.point 썸네일",
             url: "https://sunghoon1209.github.io/hpoint/",
             codeUrl: "https://github.com/sunghoon1209/hpoint/archive/refs/heads/main.zip",
             developVision: "• 주로 HTML과 CSS 구현에 중점을 두고 제작했습니다. jQuery의 경우 간단한 이벤트와 Swiper와 Slick Slider 의 플러그인을 추가하였고 이에 대한 스타일과 기능을 제어했습니다.",
@@ -151,6 +159,7 @@ window.onload = function () {
             subttitle: "Web | 퍼블리싱 100%",
             description: "Hanhwa Chemical 페이지를 클론 코딩했습니다.",
             pageThumb: "./images/hanhwafull.webp",
+            pageTumbAlt: "한화 케미칼  썸네일",
             url: "https://sunghoon1209.github.io/hanhwa/",
             codeUrl: "https://github.com/sunghoon1209/hanhwa/archive/refs/heads/main.zip",
             developVision: "HTML,CSS만 사용하여 기본적인 구조와 스타일을 제작하고 wow.js만 이용하여 등장하는 애니메이션 효과를 주었습니다.",
@@ -170,6 +179,7 @@ window.onload = function () {
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "Ground X 페이지를 Figma를 통해 가져온 후 해당 디자인 파일을 바탕으로 코딩했습니다.",
             pageThumb: "./images/groundXfull.webp",
+            pageTumbAlt: "Ground X 썸네일",
             url: "https://sunghoon1209.github.io/GroundX/",
             codeUrl: "https://github.com/sunghoon1209/GroundX/archive/refs/heads/main.zip",
             developVision: " 모바일과 PC 2단 반응형 구조로 제작했습니다. 또한 wow.js를 사용해서 fade in 애니메이션 효과를 줬습니다. 또한 jQuery로 스크롤 위치에 따라 메뉴의 색이 변경하고 해당 메뉴를 누르면 해당 스크롤로 이동하는 기능을 구현했습니다.",
@@ -187,7 +197,8 @@ window.onload = function () {
             title: "SK Picglobal",
             subttitle: "Web | 퍼블리싱 100%",
             description: "SK Picglobal 페이지를 클론 코딩했습니다.",
-            pageThumb: "./images/skPicFull.webp",
+            pageThumb: "./images/skPicfull.webp",
+            pageTumbAlt: "SkPicGlobal 썸네일",
             url: "https://sunghoon1209.github.io/skPicglobal/",
             codeUrl: "https://github.com/sunghoon1209/skPicglobal/archive/refs/heads/main.zip",
             developVision: "HTML과 CSS를 이용하여 기본적인 구조를 잘 제작하고, jQuery를 이용하여 다양한 기능을 구현해서 제작했습니다. slick과 wow 등의 플러그인을 활용해서 추가적인 기능을 쉽게 적용했습니다.",
@@ -206,6 +217,7 @@ window.onload = function () {
             subttitle: "반응형 | 퍼블리싱 100%",
             description: "리그 오브 레전드 게임 홈페이지의 게임 모드 설명하는 영역을 새롭게 디자인 하여 퍼블리싱 했습니다.",
             pageThumb: "./images/lol.webp",
+            pageTumbAlt: "league of legend 썸네일",
             url: "https://sunghoon1209.github.io/leagueoflegend/",
             codeUrl: "https://github.com/sunghoon1209/introducetovideo/archive/refs/heads/leagueoflegend.zip",
             developVision: "pc와 모바일 구조의 반응형으로 제작했습니다. SCSS의 작성법을 연습하기 위해 SCSS로 제작했습니다.",
@@ -283,6 +295,8 @@ window.onload = function () {
             popUpDes.textContent = data.description;
             // 썸네일
             popUpThumb.setAttribute("src", data.pageThumb);
+            // 썸네일 이미지 alt값
+            popUpThumb.setAttribute("alt", data.pageTumbAlt);
             // Url href변경
             popUpUrlHref.setAttribute("href", data.url);
             // 코드 다운 url 변경
@@ -335,6 +349,8 @@ window.onload = function () {
             popUpDes.textContent = data.description;
             // 썸네일
             popUpThumb.setAttribute("src", data.pageThumb);
+             // 썸네일 이미지 alt값
+             popUpThumb.setAttribute("alt", data.pageTumbAlt);
             // Url href변경
             popUpUrlHref.setAttribute("href", data.url);
             // 코드 다운 url 변경
